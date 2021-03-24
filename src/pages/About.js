2c6f082f2043faecef6725562cce25729c16b4b1
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-03-24 14:36:10 -0300

    new: add basic setup to show

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum esse
+        soluta fuga incidunt molestiae magnam blanditiis vel perferendis iste
+        ipsa, corrupti vitae in eveniet molestias voluptate repellendus ex
+        laborum! Assumenda!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
