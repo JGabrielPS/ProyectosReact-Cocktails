a79396bc40e73db0d410663caa006bd5a767479d
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-03-24 14:34:53 -0300

    new: add setSearchTerm from global context, useRef for input to take values that set searchTerm state, add handleSubmit to prevent reload page when press enter, and useEffect to set focus in input when render for first time

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,37 @@
-import React from 'react'
-import { useGlobalContext } from '../context'
+import React, { useRef, useEffect } from "react";
+import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
-  return (
-    <div>
-      <h2>search form component</h2>
-    </div>
-  )
-}
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = useRef("");
 
-export default SearchForm
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  return (
+    <div className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search your favourite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchForm;
